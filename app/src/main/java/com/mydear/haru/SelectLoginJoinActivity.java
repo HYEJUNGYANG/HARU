e4994424ae716dee5330c9aa84c5a1c872bc17cb
양혜정 <gpwjd248@gmail.com>
2022-10-11 22:15:46 +0900

    첫 실행화면 - 디자인 및 상태바 투명으로 지정

--- app/src/main/java/com/mydear/haru/SelectLoginJoinActivity.java
+++ app/src/main/java/com/mydear/haru/SelectLoginJoinActivity.java
@@ -1,18 +1,26 @@
 package com.mydear.haru;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.ActionBar;
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class SelectLoginJoinActivity extends AppCompatActivity {
 
-    private TextView textView;
 
     private Dialog surveyDialog;
 
@@ -21,56 +29,43 @@ public class SelectLoginJoinActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_login_join);
 
-        textView = findViewById(R.id.textView);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);  // title 제거
 
-        surveyDialog = new Dialog(SelectLoginJoinActivity.this);  // 초기화
-        surveyDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);  // 타이틀 제거
-        surveyDialog.setContentView(R.layout.activity_survey_dialog); // xml 레이아웃
+        // 상태바 투명처리
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
-        showSurveyDialog();
+//        surveyDialog = new Dialog(SelectLoginJoinActivity.this);  // 초기화
+//        surveyDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);  // 타이틀 제거
+//        surveyDialog.setContentView(R.layout.activity_survey_dialog); // xml 레이아웃
 
-        Button btn_open = findViewById(R.id.btn_open);
-        btn_open.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showSurveyDialog();
-            }
-        });
-
-        Button btn_test = findViewById(R.id.btn_test);
-        btn_test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SelectLoginJoinActivity.this, ProductInfoActivity.class);
-                startActivity(intent);
-            }
-        });
-
+//        showSurveyDialog();
     }
 
-    public void showSurveyDialog() {
-        surveyDialog.show();
-
-        // 주의사항!! 여기서는 findViewById 앞에 다이얼로그 이름을 붙여줘야 한다
-
-        Button btn_yes = surveyDialog.findViewById(R.id.btn_yes);
-        btn_yes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SelectLoginJoinActivity.this, MainActivity.class);
-                startActivity(intent);
-                surveyDialog.dismiss();
-            }
-        });
-
-        Button btn_no = surveyDialog.findViewById(R.id.btn_no);
-        btn_no.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                surveyDialog.dismiss();
-            }
-        });
-    }
+//    public void showSurveyDialog() {
+//        surveyDialog.show();
+//
+//        // 주의사항!! 여기서는 findViewById 앞에 다이얼로그 이름을 붙여줘야 한다
+//
+//        Button btn_yes = surveyDialog.findViewById(R.id.btn_yes);
+//        btn_yes.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(SelectLoginJoinActivity.this, MainActivity.class);
+//                startActivity(intent);
+//                surveyDialog.dismiss();
+//            }
+//        });
+//
+//        Button btn_no = surveyDialog.findViewById(R.id.btn_no);
+//        btn_no.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                surveyDialog.dismiss();
+//            }
+//        });
+//    }
 }
 
 
