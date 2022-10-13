308d3121b099d38a303677e0d08991023cde4139
양혜정 <gpwjd248@gmail.com>
2022-10-13 17:13:30 +0900

    로그인 화면 - 값 입력에 따른 버튼 활성화 및 입력 텍스트 활성화

--- app/src/main/java/com/mydear/haru/LoginActivity.java
+++ app/src/main/java/com/mydear/haru/LoginActivity.java
@@ -7,13 +7,30 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView tv_id_filled;
+    private TextView tv_pw_filled;
     private TextView tv_join;
+    private EditText et_id;
+    private EditText et_pw;
+
+    private Button btn_login;
+
+    private String str_id;
+    private String str_pw;
+    private boolean id_value = false;
+    private boolean pw_value = false;
+    private boolean value = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +43,90 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);  // title 제거
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        tv_id_filled = findViewById(R.id.tv_id_filled);
+
+        tv_pw_filled = findViewById(R.id.tv_pw_filled);
+
         tv_join = findViewById(R.id.tv_join);
         tv_join.setPaintFlags(tv_join.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);  // 텍스트 밑줄
         tv_join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 goToJoinActivity();
+            }
+        });
+
+        et_id = findViewById(R.id.et_id);
+        et_id.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                str_id = et_id.getText().toString();
+                if (str_id.length() != 0) {
+                    id_value = true;
+                    tv_id_filled.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    id_value = false;
+                }
+                checkAllValue();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        et_id.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (!b && !id_value) {
+                    tv_id_filled.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        et_pw = findViewById(R.id.et_pw);
+        et_pw.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                str_pw = et_pw.getText().toString();
+                if (str_pw.length() != 0) {
+                    pw_value = true;
+                    tv_pw_filled.setVisibility(View.INVISIBLE);
+                } else {
+                    pw_value = false;
+                }
+                checkAllValue();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        et_pw.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (!b && !pw_value) {
+                    tv_pw_filled.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "로그인", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -41,6 +136,18 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
         overridePendingTransition(0, 0);
         finish();
+    }
+
+    public void checkAllValue() {
+        value = id_value && pw_value;
+        if (value) {
+            btn_login.setEnabled(true);
+            btn_login.setBackground(getDrawable(R.drawable.edit_rect_navi_loginver));
+        }
+        else {
+            btn_login.setEnabled(false);
+            btn_login.setBackground(getDrawable(R.drawable.edit_rect_gray));
+        }
     }
 
     @Override
